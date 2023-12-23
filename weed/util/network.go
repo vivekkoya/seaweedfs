8cfb8e5b975e9539c9950ec35ec2fb95e2bc9c6f
vivekkoya <67130044+vivekkoya@users.noreply.github.com>
2023-12-23 13:18:28 -0800

    Update network.go and remove commented code

--- weed/util/network.go
+++ weed/util/network.go
@@ -19,14 +19,9 @@ func DetectedHostAddress() string {
 		return v4Address
 	}
 
-	// if v6Address := selectIpV4(netInterfaces, false); v6Address != "" {
-	// 	return v6Address
-	// }
-
 	return "localhost"
 }
 func selectIpV4(netInterfaces []net.Interface) string {
-// func selectIpV4(netInterfaces []net.Interface, isIpV4 bool) string {
 	for _, netInterface := range netInterfaces {
 		if (netInterface.Flags & net.FlagUp) == 0 {
 			continue
@@ -38,15 +33,9 @@ func selectIpV4(netInterfaces []net.Interface) string {
 
 		for _, a := range addrs {
 			if ipNet, ok := a.(*net.IPNet); ok && !ipNet.IP.IsLoopback() {
-				// if isIpV4 {
-				// 	if ipNet.IP.To4() != nil {
-				// 		return ipNet.IP.String()
-				// 	}
-				// } else {
 					if ipNet.IP.To16() != nil {
 						return ipNet.IP.String()
 					}
-				// }
 			}
 		}
 	}
