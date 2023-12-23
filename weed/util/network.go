dc658babb8705976f56a02354770f51866b9950c
vivekkoya <67130044+vivekkoya@users.noreply.github.com>
2023-12-23 13:16:24 -0800

    Update network.go since To16 converts the IP address ip up to a 16-byte representation. If ip is not an IP address (it is the wrong length), To16 returns nil.
    To16 works also for ipv4 addresses as referenced in the go documentation: https://pkg.go.dev/net

--- weed/util/network.go
+++ weed/util/network.go
@@ -15,18 +15,18 @@ func DetectedHostAddress() string {
 		return ""
 	}
 
-	if v4Address := selectIpV4(netInterfaces, true); v4Address != "" {
+	if v4Address := selectIpV4(netInterfaces); v4Address != "" {
 		return v4Address
 	}
 
-	if v6Address := selectIpV4(netInterfaces, false); v6Address != "" {
-		return v6Address
-	}
+	// if v6Address := selectIpV4(netInterfaces, false); v6Address != "" {
+	// 	return v6Address
+	// }
 
 	return "localhost"
 }
-
-func selectIpV4(netInterfaces []net.Interface, isIpV4 bool) string {
+func selectIpV4(netInterfaces []net.Interface) string {
+// func selectIpV4(netInterfaces []net.Interface, isIpV4 bool) string {
 	for _, netInterface := range netInterfaces {
 		if (netInterface.Flags & net.FlagUp) == 0 {
 			continue
@@ -38,15 +38,15 @@ func selectIpV4(netInterfaces []net.Interface, isIpV4 bool) string {
 
 		for _, a := range addrs {
 			if ipNet, ok := a.(*net.IPNet); ok && !ipNet.IP.IsLoopback() {
-				if isIpV4 {
-					if ipNet.IP.To4() != nil {
-						return ipNet.IP.String()
-					}
-				} else {
+				// if isIpV4 {
+				// 	if ipNet.IP.To4() != nil {
+				// 		return ipNet.IP.String()
+				// 	}
+				// } else {
 					if ipNet.IP.To16() != nil {
 						return ipNet.IP.String()
 					}
-				}
+				// }
 			}
 		}
 	}
